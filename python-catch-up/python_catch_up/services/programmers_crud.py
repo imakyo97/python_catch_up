bc32b48aa94f0ddfec11180592f2e1a1534ed622
imakyo97 <kyoheishogun.1997@gmail.com>
2024-02-21 13:34:17 +0900

    Pylanceの警告を修正

--- python-catch-up/python_catch_up/services/programmers_crud.py
+++ python-catch-up/python_catch_up/services/programmers_crud.py
@@ -1,11 +1,11 @@
-from typing import List, Type
+from typing import List
 from models import models
 from schemas import schemas
 from schemas.error_response import MyException
 
-async def __to_schema_programmer(programmer: Type[models.Programmer]) -> schemas.Programmer:
-    technologies = [schemas.Technology(id=i.id, name=i.name) async for i in programmer.technologies] 
-    programmer = schemas.Programmer(id=programmer.id, name=programmer.name, technologies=technologies)
+async def __to_schema_programmer(orm_programmer: models.Programmer) -> schemas.Programmer:
+    technologies = [schemas.Technology(id=i.id, name=i.name) async for i in orm_programmer.technologies] 
+    programmer = schemas.Programmer(id=orm_programmer.id, name=orm_programmer.name, technologies=technologies)
     return programmer
 
 async def get_programmers() -> List[schemas.Programmer]:
@@ -24,7 +24,7 @@ async def get_programmers() -> List[schemas.Programmer]:
         # technologies = [schemas.Technology(id=i.id, name=i.name) async for i in orm_programmer.technologies] 
         # programmer = schemas.Programmer(id=orm_programmer.id, name=orm_programmer.name, technologies=technologies)
 
-        programmer = await __to_schema_programmer(programmer=orm_programmer)
+        programmer = await __to_schema_programmer(orm_programmer=orm_programmer)
         result.append(programmer)
     return result
 
@@ -36,7 +36,7 @@ async def get_programmer(programmer_id: int) -> schemas.Programmer:
             title="ユーザが存在しません",
             message="画面を更新して、再度やり直してください"
         )
-    programmer = await __to_schema_programmer(programmer=orm_programmer)
+    programmer = await __to_schema_programmer(orm_programmer=orm_programmer)
     return programmer
 
 async def create_programmer(programmer_data: schemas.ProgrammerData) -> schemas.Programmer:
@@ -44,7 +44,7 @@ async def create_programmer(programmer_data: schemas.ProgrammerData) -> schemas.
     for technology_name in programmer_data.technologies:
         technology, created = await models.Technology.get_or_create(name=technology_name)
         await programmer.technologies.add(technology)
-    programmer = await __to_schema_programmer(programmer=programmer)
+    programmer = await __to_schema_programmer(orm_programmer=programmer)
     return programmer
 
 async def update_programmer(programmer_id: int, programmer_data: schemas.ProgrammerData) -> schemas.Programmer:
@@ -61,7 +61,7 @@ async def update_programmer(programmer_id: int, programmer_data: schemas.Program
         technology, created = await models.Technology.get_or_create(name=technology_name)
         await programmer.technologies.add(technology)
     await programmer.save()
-    return await __to_schema_programmer(programmer=programmer)
+    return await __to_schema_programmer(orm_programmer=programmer)
 
 async def delete_programmer(programmer_id: int) -> schemas.Programmer:
     programmer = await models.Programmer.get_or_none(id=programmer_id)
@@ -71,6 +71,6 @@ async def delete_programmer(programmer_id: int) -> schemas.Programmer:
             title="ユーザが存在しません",
             message="画面を更新して、再度やり直してください"
         )
-    cache = await __to_schema_programmer(programmer=programmer)
+    cache = await __to_schema_programmer(orm_programmer=programmer)
     await programmer.delete()
     return cache
